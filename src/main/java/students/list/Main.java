5d5ac0332d632634ccbc08e7ce5bad7b4ffd6f92
NazarBunko <bunkonazar@gmail.com>
2024-06-28 12:23:03 +0300

    5

--- src/main/java/students/list/Main.java
+++ src/main/java/students/list/Main.java
@@ -3,25 +3,38 @@ package students.list;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 @Component
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
 
-        Properties properties = new Properties();
-        InputStream input = Files.newInputStream(Paths.get(".properties"));
-        properties.load(input);
+        int windowWidth = context.getBean("windowWidth", Integer.class);
+        int windowHeight = context.getBean("windowHeight", Integer.class);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         MainWindow window = context.getBean("mainWindow", MainWindow.class);
-        window.setSize(Integer.parseInt(properties.getProperty("${mainWindow.width}")), Integer.parseInt(properties.getProperty("${mainWindow.height}")));
+        window.setSize(windowWidth, windowHeight);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
         context.close();
     }
